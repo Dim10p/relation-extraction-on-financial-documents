cf2e4e07cc60beb79c671f96fde7d16ff060cf58
Dim10p <petridis.dim@outlook.com>
2023-07-10 12:26:30 +0000

    tmp

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,30 @@
-##d
+"""
+This file contains all the constants that are used in this project.
+"""
+
+# import necessary libraries/modules:
+import os
+
+
+mapping = { "no_relation": 0,
+            "org:date:formed_on" : 1,
+            "org:gpe:operations_in" : 2,
+            "pers:org:member_of" : 3,
+            "pers:org:employee_of" : 4,
+            "pers:gov_agy:member_of" : 5,
+            "org:org:acquired_by" : 6,
+            "org:money:loss_of" : 7,
+            "org:gpe:headquartered_in" : 8,
+            "pers:univ:employee_of" : 9,
+            "org:date:acquired_on" : 10,
+            "pers:univ:attended" : 11,
+            "org:gpe:formed_in" : 12,
+            "org:money:profit_of" : 13,
+            "org:money:cost_of" : 14,
+            "org:org:subsidiary_of" : 15,
+            "org:org:shares_of" : 16,
+            "pers:org:founder_of" : 17,
+            "pers:title:title" : 18,
+            "org:money:revenue_of" : 19,
+            "org:org:agreement_with" : 20,
+            "pers:univ:member_of":21}
--- utils/utility_functions.py
+++ utils/utility_functions.py
@@ -5,3 +5,15 @@ def convert_to_list(value):
         return ast.literal_eval(value)
     except (SyntaxError, ValueError):
         return value
+
+def create_input(token_list, e1_start, e2_start, ner_list):
+  output_text = ""
+  for i in range(0, len(token_list)):
+    if i == e1_start:
+      text = str(ner_list[i]) + " "+ str(token_list[i])
+    elif i == e2_start:
+      text = str(ner_list[i]) + " "+ str(token_list[i])
+    else:
+      text = token_list[i]
+    output_text= output_text + " " + text
+  return output_text
